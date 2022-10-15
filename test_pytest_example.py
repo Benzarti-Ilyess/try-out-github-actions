3538a059157890261c22c22d06d526eba8cd7922
Ilyess Benzarti <milyess.benzarti@gmail.com>
2022-10-15 23:54:53 +0200

    Integrate Black Formatting check (#3)
    * Integrate Black Formatting check

--- test_pytest_example.py
+++ test_pytest_example.py
@@ -3,8 +3,7 @@ import sys
 import pytest
 
 
-@pytest.mark.skipif(sys.version_info < (3,6), reason="requires python3.6")
+@pytest.mark.skipif(sys.version_info < (3, 6), reason="requires python3.6 or above")
 def test_assert_true():
-  
-  assert 1==1
-  
+
+    assert 1 == 1
