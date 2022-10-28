f91886a3d78740b5196bbc5d1d25d300987529d0
Ilyess Benzarti <milyess.benzarti@gmail.com>
2022-10-28 09:04:17 +0200

    Revert

--- test_pytest_example.py
+++ test_pytest_example.py
@@ -1,9 +1,9 @@
-import pytest
 import sys
 
-import os
+import pytest
 
 
 @pytest.mark.skipif(sys.version_info < (3, 6), reason="requires python3.6 or above")
 def test_assert_true():
+
     assert 1 == 1
