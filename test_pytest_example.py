7c31cd3ef5c40b35e8446ec531cc614a9d26cc4b
Ilyess Benzarti <milyess.benzarti@gmail.com>
2022-10-28 08:29:48 +0200

    Invoke error on the pipeline

--- test_pytest_example.py
+++ test_pytest_example.py
@@ -1,9 +1,9 @@
+import pytest
 import sys
 
-import pytest
+import os
 
 
 @pytest.mark.skipif(sys.version_info < (3, 6), reason="requires python3.6 or above")
 def test_assert_true():
-
     assert 1 == 1
