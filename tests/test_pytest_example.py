2df63e382816e5c5b9a8ce533a7d4353a9d281a1
Ilyess Benzarti <milyess.benzarti@gmail.com>
2022-10-28 15:40:26 +0200

    fix test

--- tests/test_pytest_example.py
+++ tests/test_pytest_example.py
@@ -1,7 +1,6 @@
-from example import assert_true
-
 import pytest
 
+from src.example import assert_true
 
 @pytest.mark.skipif(sys.version_info < (3, 6), reason="requires python3.6 or above")
 def test_assert_true():
